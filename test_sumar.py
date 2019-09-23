08b9f060226eb87d38574692b3b9a825ce32cd28
Jonatanavila <42453149+Jonatanavila@users.noreply.github.com>
2019-09-23 16:57:17 -0300

    Update test_sumar.py

--- test_sumar.py
+++ test_sumar.py
@@ -8,7 +8,7 @@ from unittest.mock import patch
 class Test_sut(unittest.TestCase):
 
     def test_sumar(self):
-        self.assertEqual(sut.sumar(2,4),2)
+        self.assertEqual(sut.sumar(2,4),6)
 
     def test_saludar(self):
         self.assertTrue(sut.saludar('hola'+'nombre'),'hola joni')
@@ -51,12 +51,23 @@ class Test_sut(unittest.TestCase):
 
     @patch('math.exp')
     @patch('math.sqrt')
-    def supercalc(self,exp,sqrt):
+    def supercalc(self, exp, sqrt):
         exp.return_value=2
         sqrt.return_value=2
-        a=sut.supercalc(3)
-        self.assertTrue(a == 2)
-        return sqrt	
+        a= sut.supercalc(3)
+        self.assertTrue(a == 2)	
+	
+    def test_CalcuClass_sum(self):
+        calc = sut.CalcuClass().suma(a=7, b=4)
+        self.assertTrue(calc == 11)
+        
+    def test_CalcuClass_pro(self):
+        calcu = sut.CalcuClass().producto(a=5, b=5)
+        self.assertTrue(calcu == 25)
+        
+    def test_CalcuClass_list(self):
+        calcu = sut.CalcuClass().sumar_varios(lista = [4,9,3])
+        self.assertTrue(calcu == 16)
 
 if __name__ == '__main__':
     unittest.main()
